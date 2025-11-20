5f31a6ef09d1719bc72e9b67852d3394dfed3c7f
lucaskordi <lucas.kordiaka@gmail.com>
2025-11-20 18:20:43 -0300

    Adjust hero section positioning: Changed bottom margin from 8 to 24 for improved layout on larger screens.

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -95,7 +95,7 @@ export default function HeroSection() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1, delay: 0.8 }}
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-20"
+        className="absolute bottom-24 sm:bottom-8 left-1/2 transform -translate-x-1/2 z-20"
       >
         <motion.div
           animate={{ y: [0, 10, 0] }}
